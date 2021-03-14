d9dd6c90633e4bf554e4c33ec322544467df0f89
陈贽 <chenzhi2017@xinhua.org>
2021-03-15 01:12:57 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { createApp } from 'vue';
 import 'normalize.css';
-import 'element-plus/lib/theme-chalk/index.css';
-import {
-  ElLoading,
-  ElMessage,
-} from 'element-plus';
+// import 'element-plus/lib/theme-chalk/index.css';
+// import {
+//   ElLoading,
+//   ElMessage,
+// } from 'element-plus';
 import App from '/@/app.vue';
 import router from '/@/router';
 import '/@/styles/common.scss';
 
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $message: typeof ElMessage;
-  }
-}
+// declare module '@vue/runtime-core' {
+//   interface ComponentCustomProperties {
+//     $message: typeof ElMessage;
+//   }
+// }
 
 const app = createApp(App);
 app.use(router);
-app.use(ElLoading);
-app.use(ElMessage as any); // type bug
+// app.use(ElLoading);
+// app.use(ElMessage as any); // type bug
 app.mount('#app');
